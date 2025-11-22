02c81cc65d0eb2e87d370579f2af05fcf5cd454e
Ganesh Kadam <kadamganesh18504@gmail.com>
2025-11-22 15:59:09 +0530

    Add files via upload
    mini proj_1_color_app

---
+++ script.js
@@ -0,0 +1,22 @@
+
+
+document.getElementById("red").onclick = () =>{
+    document.body.style.background = "red";
+
+}
+document.getElementById("green").onclick = () =>{
+    document.body.style.background = "green";
+
+}
+document.getElementById("blue").onclick = () =>{
+    document.body.style.background = "blue";
+}
+document.getElementById("orange").onclick = () =>{
+    document.body.style.background = "orange";
+}
+document.getElementById("purple").onclick = () =>{
+    document.body.style.background = "purple";
+}
+document.getElementById("black").onclick = () =>{
+    document.body.style.background = "black";
+}
